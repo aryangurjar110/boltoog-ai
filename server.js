42cc508bcf691eb11c4a59fb509d9bd25b0759fb
aryangurjar110 <aryangurjar110@gmail.com>
2026-05-15 16:44:16 +0530

    Fix: infinite redirect loop on login

--- server.js
+++ server.js
@@ -67,6 +67,11 @@ app.post('/chat', async (req, res) => {
 
 // Serve static files
 app.use(express.static(path.join(__dirname)));
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
